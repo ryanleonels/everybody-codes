ec2cdfcc7986c6e1cc1225fc6519139b40d74986
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-11-04 08:05:12 +0000

    add directory structure for 2025 quests + solution for 2025 day 1

---
+++ 2025/1/1b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+fileHandle = open("everybody_codes_e2025_q01_p2.txt", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+fileLines = fileData.split('\n')
+names = fileLines[0].split(',')
+instructions = fileLines[2].split(',')
+(n, n1, pos) = (len(names), len(instructions), 0)
+for i in range(0, n1):
+	step = int(instructions[i][1:])
+	if instructions[i][0] == 'L':
+		step = -step
+	pos += step
+	pos = (pos % n)
+print(names[pos])
